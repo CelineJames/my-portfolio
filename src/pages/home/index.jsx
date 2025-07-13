50f330fe94b0987934876c216852b1e19b0f47a4
Itoro James <kaytorah55@gmail.com>
2025-07-13 14:33:47 -0700

    debugging

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import Hero from "./hero";
 
 export default function Home() {
   return (
-    <div className="bg-primary dark:bg-secondary">
+    <div className="relative overflow bg-primary dark:bg-secondary">
       <Hero />
     </div>
   );
