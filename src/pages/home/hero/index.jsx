b59d7143096b459759102f09c419d9d2ec44a687
Itoro James <kaytorah55@gmail.com>
2025-07-27 13:08:17 -0700

    new changes

--- src/pages/home/hero/index.jsx
+++ src/pages/home/hero/index.jsx
@@ -80,7 +80,7 @@ export default function Hero() {
   }, [currentIndex, words.length]);
 
   return (
-    <div className=" overflow-y-hidden relative flex flex-row md:justify-center items-center bg-primary dark:bg-secondary">
+    <div className="h-screen overflow-y-hidden relative flex flex-row md:justify-center items-center bg-primary dark:bg-secondary">
       {/* Left Section (Hero) with slant leaning into black section */}
 
       <section
