1aa736007584f597f85444a15d9d95446b4dc0e8
Itoro James <kaytorah55@gmail.com>
2025-07-27 13:04:15 -0700

    new changes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,7 @@ import Hero from "./hero";
 
 export default function Home() {
   return (
-    <div className="relative overflow-y-hidden bg-primary dark:bg-secondary">
+    <div className="relative overflow-y-hidden h-screen bg-primary dark:bg-secondary">
       <Hero />
     </div>
   );
