5f36eeb8686c1f359e52179dbe49141580f06faf
Itoro James <kaytorah55@gmail.com>
2025-12-18 21:53:31 +0100

    modified resume

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -9,7 +9,7 @@ const ResumeViewer = () => {
         className="max-h-[80vh] w-auto object-contain shadow-lg rounded mt-14"
       />
       <a
-        href="/Itoro-James.pdf"
+        href="/itoro_james.pdf"
         download
         className="px-5 py-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
       >
