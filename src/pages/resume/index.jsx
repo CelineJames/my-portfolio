88401ecd41e47378ed79b27f209c9160f09e4a1b
Itoro James <kaytorah55@gmail.com>
2026-01-06 11:51:30 +0100

    Fix overflow of resume file

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,13 +1,13 @@
-// import resume from "@/itoro_james.pdf";
-
 const ResumeViewer = () => {
   return (
-    <div className="h-[100dvh] w-full pt-14">
-      <iframe
-        src="/itoro_james.pdf"
-        className="h-full w-full rounded shadow-lg"
-        title="Resume"
-      />
+    <div className="h-[100dvh] w-full pt-14 overflow-hidden flex justify-center">
+      <div className="h-full w-full md:w-[80%] lg:w-[70%] overflow-hidden">
+        <iframe
+          src="/itoro_james.pdf#view=FitH"
+          className="h-full w-full border-0"
+          title="Resume"
+        />
+      </div>
     </div>
   );
 };
